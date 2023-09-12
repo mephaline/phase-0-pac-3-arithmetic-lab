06795a2bf127d2894b7a5b0f49c4c9ed10185216
mephaline <suaramos88@gmail.com>
2023-09-12 00:52:14 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return a + b;
+  }
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return n + 1;
+  }
+function decrement(n) {
+    return n - 1;
+  }
+function makeInt(n) {
+    const parsedValue = parseInt(n, 10);
+    return parsedValue;
+  }
+function preserveDecimal(n) {
+    const parsedValue = parseFloat(n);
+    return parsedValue;
+  }
